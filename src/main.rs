7fe3d38ccea42a62f0ea75c4053c16e06b1496eb
tenheadedlion <no-email>
2021-02-25 13:25:11 +0800

    MOdify sth.

--- src/main.rs
+++ src/main.rs
@@ -2,70 +2,49 @@ use std::io::{self, Read};
 use ssvm_tensorflow_interface;
 use serde::Deserialize;
 
+fn search_vec(vector: &Vec<f32>, labels: &Vec<&str>, value: &f32) -> (i32, String) {
+    for (i, f) in vector.iter().enumerate() {
+        if f == value {
+            return (i as i32, labels[i].to_owned());
+        }
+    }
+    return (-1, "Unclassified".to_owned());
+}
+
 fn main() {
-    let model_data: &[u8] = include_bytes!("lite-model_aiy_vision_classifier_food_V1_1.tflite");
-    let labels = include_str!("aiy_food_V1_labelmap.txt");
+    let model_data: &[u8] = include_bytes!("mobilenet_v2_1.4_224_frozen.pb");
+    let labels = include_str!("imagenet_slim_labels.txt");
+    let label_lines : Vec<&str> = labels.lines().collect();
+
 
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).expect("Error reading from STDIN");
     let obj: FaasInput = serde_json::from_str(&buffer).unwrap();
-    // println!("{} {}", &(obj.body)[..5], obj.body.len());
     let img_buf = base64::decode_config(&(obj.body), base64::STANDARD).unwrap();
-    // println!("Image buf size is {}", img_buf.len());
 
-    let flat_img = ssvm_tensorflow_interface::load_jpg_image_to_rgb8(&img_buf, 192, 192);
+    let flat_img = ssvm_tensorflow_interface::load_jpg_image_to_rgb32f(&img_buf, 224, 224);
 
-    let mut session = ssvm_tensorflow_interface::Session::new(&model_data, ssvm_tensorflow_interface::ModelType::TensorFlowLite);
-    session.add_input("input", &flat_img, &[1, 192, 192, 3])
+    let mut session = ssvm_tensorflow_interface::Session::new(model_data, ssvm_tensorflow_interface::ModelType::TensorFlow);
+    session.add_input("input", &flat_img, &[1, 224, 224, 3])
+           .add_output("MobilenetV2/Predictions/Softmax")
            .run();
-    let res_vec: Vec<u8> = session.get_output("MobilenetV1/Predictions/Softmax");
+    let res_vec: Vec<f32> = session.get_output("MobilenetV2/Predictions/Softmax");
+    let mut sorted_vec = res_vec.clone(); 
+    sorted_vec.sort_by(|a, b| b.partial_cmp(a).unwrap());
 
-    let mut i = 0;
-    let mut max_index: i32 = -1;
-    let mut max_value: u8 = 0;
-    while i < res_vec.len() {
-        let cur = res_vec[i];
-        if cur > max_value {
-            max_value = cur;
-            max_index = i as i32;
-        }
-        i += 1;
-    }
-    // println!("{} : {}", max_index, max_value as f32 / 255.0);
+    let top1 = sorted_vec[0];
+    let top2 = sorted_vec[1];
+    let top3 = sorted_vec[2];
 
-    /*
-    let mut confidence = "could be";
-    if max_value > 200 {
-        confidence = "is very likely";
-    } else if max_value > 125 {
-        confidence = "is likely";
-    } else if max_value > 50 {
-        confidence = "could be";
-    }
-    */
-    let mut confidence = "可能有";
-    if max_value > 200 {
-        confidence = "非常可能有";
-    } else if max_value > 125 {
-        confidence = "很可能有";
-    } else if max_value > 50 {
-        confidence = "可能有";
-    }
+    let r1 = search_vec(&res_vec, &label_lines, &top1);
+    let r2 = search_vec(&res_vec, &label_lines, &top2);
+    let r3 = search_vec(&res_vec, &label_lines, &top3);
 
-    let mut label_lines = labels.lines();
-    for _i in 0..max_index {
-      label_lines.next();
-    }
-
-    let class_name = label_lines.next().unwrap().to_string();
-    if max_value > 50 && max_index != 0 {
-      // println!("It {} a <a href='https://www.google.com/search?q={}'>{}</a> in the picture", confidence.to_string(), class_name, class_name);
-      println!("上传的图片里面{} <a href='https://www.google.com/search?q={}'>{}</a>", confidence.to_string(), class_name, class_name);
-    } else {
-      // println!("It does not appears to be any food item in the picture.");
-      println!("上传的图片里面没有检测到食品");
-    }
-    // println!("{} : {}", label_lines.next().unwrap().to_string(), confidence.to_string());
+    println!("{}: {:.2}%\n{}: {:.2}%\n{}: {:.2}%"
+        , r1.1, top1 * 100.0
+        , r2.1, top2 * 100.0   
+        , r3.1, top3 * 100.0   
+        );
 }
 
 #[derive(Deserialize, Debug)]
